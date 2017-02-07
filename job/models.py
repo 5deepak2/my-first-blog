43d4bfd8bb12883899906ebd9e919f1f7c4a8da7
5deepak2 <deepak25xiss@gmail.com>
2017-02-07 13:55:34 +0530

    new changes to models

--- job/models.py
+++ job/models.py
@@ -1,5 +1,30 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Company(models.Model):
+    company_name=models.CharField(max_length=100)
+    company_description=models.TextField(max_length=200)
+
+class Category(models.Model):
+    taxonomy=models.CharField(max_length=100)
+    taxoterms=models.CharField(max_length=100)
+
+class JobPost(models.Model):
+    organisation=models.ForeignKey(Company)
+    position=models.CharField(max_length=100)
+    description=models.TextField()
+    city=models.CharField(max_length=100)
+    category=models.ForeignKey(Category,default='all')
+
+
+    def __str__(self):
+        return self.postion
+
+
+
+
+
+
+
+
--- job/views.py
+++ job/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import JobPost
 
-# Create your views here.
+def job_list(request):
+    jobs=JobPost.objects.all()
+    return render(request,'jobs/job_list.html',{jobs:jobs})
